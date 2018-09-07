f1235efb8bdcdbaa9b14596339b30a723f21ee6c
fizzysuleman <41524187+fizzysuleman@users.noreply.github.com>
2018-09-07 09:32:22 -0700

    Add files via upload

---
+++ assessment2.js
@@ -0,0 +1,38 @@
+var jambScore=299
+
+
+if (jambScore >= 214) {
+    console.log("CONVENANT UNIVERSITY:You can be admitted CU");
+}
+else{
+    console.log("CONVENANT UNIVERSITY:You cannot gain admission into any univeristy, try again next year");
+}
+
+if(jambScore>=265){
+    console.log("FUTA:You can be admitted to FUTA");
+}
+else{
+    console.log("FUTA:You got lower than the passmark ")
+}
+if (jambScore>=275){
+    console.log("OAU:You can be admitted into OAU")
+}
+else{
+    console.log("OAU:You got lower than the passmark")
+}
+
+if (jambScore>=280){
+    console.log("University of ilorin:You can be admitted into UI")
+}
+else{
+    console.log("Univeristy of Ilorin: You got lower than the passmark")
+}
+if (jambScore>=290){
+    console.log("UNILAG: You can be admitted into UNILAG")
+}
+else{
+    console.log("UNILAG: You got lower than the passmark")
+}
+
+
+
